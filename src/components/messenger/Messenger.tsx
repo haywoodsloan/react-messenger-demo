aee1104280a0b56037bea06a78707246709b62fe
Sloan Haywood <haywood.sloan@yahoo.com>
2022-11-03 11:40:46 -0400

    add router and start refactor

---
+++ src/components/messenger/Messenger.tsx
@@ -0,0 +1,7 @@
+import './Messenger.scss';
+
+function Messenger() {
+  return <div className="Messenger">Hello World!</div>;
+}
+
+export default Messenger;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { Metric } from 'web-vitals';
 
 const container = document.getElementById('root');
 if (!container) {
+  // Should also log error to analytics service
   throw new Error('Failed to find root container');
 }
 
@@ -16,12 +19,17 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendAnalytics(metric: Metric) {
+  // Eventually this should send the data to an analytics service
+  // like App Insights for now just log to the console
+  console.log(metric);
+}
+
+reportWebVitals(sendAnalytics);
