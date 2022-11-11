4251adb2d98a88f2d007aefaad0b2929fa55e1f1
Sloan Haywood <haywood.sloan@yahoo.com>
2022-11-11 18:07:34 -0500

    finish work on input

--- src/components/core/input/Input.tsx
+++ src/components/core/input/Input.tsx
@@ -1,6 +1,12 @@
 import classNames from 'classnames';
-import lineHeight from 'line-height';
-import { ChangeEventHandler, useEffect, useRef } from 'react';
+import getLineHeight from 'line-height';
+import {
+  ChangeEventHandler,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import colors from 'src/styles/colors.module.scss';
 import layout from 'src/styles/layout.module.scss';
@@ -25,42 +31,48 @@ export default function Input({
   const containerRef = useRef<HTMLDivElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  let baseLineHeight: number;
-  let verticalPadding: number;
+  const [lineHeight, setLineHeight] = useState(0);
+  const [verticalPadding, setVerticalPadding] = useState(0);
 
-  const setAutoHeight = () => {
-    if (!containerRef.current || !textAreaRef.current) {
+  useEffect(() => {
+    if (!textAreaRef.current || !containerRef.current) {
       return;
     }
 
-    if (!baseLineHeight) {
-      baseLineHeight = lineHeight(containerRef.current);
+    setLineHeight(getLineHeight(containerRef.current));
+    if (document.fonts.status !== 'loaded') {
+      document.fonts.ready.then(() => {
+        if (containerRef.current) {
+          setLineHeight(getLineHeight(containerRef.current));
+        }
+      });
     }
 
-    if (!verticalPadding) {
-      const computedStyles = getComputedStyle(textAreaRef.current);
-      verticalPadding =
-        parseInt(computedStyles.paddingBottom) +
-        parseInt(computedStyles.paddingTop);
+    const computedStyles = getComputedStyle(textAreaRef.current);
+    setVerticalPadding(
+      parseInt(computedStyles.paddingBottom) +
+        parseInt(computedStyles.paddingTop)
+    );
+  }, []);
+
+  useLayoutEffect(() => {
+    if (!textAreaRef.current) {
+      return;
     }
 
     textAreaRef.current.style.height = '0';
     textAreaRef.current.style.overflowY = 'hidden';
-    const typeableHeight = textAreaRef.current.scrollHeight - verticalPadding;
-    const lineCount = typeableHeight / baseLineHeight;
+
+    const coreHeight = textAreaRef.current.scrollHeight - verticalPadding;
+    const lineCount = coreHeight / lineHeight;
 
     if (lineCount > maxLines) {
       textAreaRef.current.style.overflowY = 'auto';
-      textAreaRef.current.style.height = `${maxLines * baseLineHeight}px`;
+      textAreaRef.current.style.height = `${maxLines * lineHeight}px`;
     } else {
-      textAreaRef.current.style.height = `${lineCount * baseLineHeight}px`;
+      textAreaRef.current.style.height = `${lineCount * lineHeight}px`;
     }
-  };
-
-  // in case the fonts aren't loaded yet
-  // re-calc the height when they are
-  document.fonts.ready.then(setAutoHeight);
-  useEffect(setAutoHeight, [value]);
+  }, [value, lineHeight, verticalPadding]);
 
   return (
     <div
