1d77a3a413f406589b217e3ca70fae024a160e60
Sloan Haywood <haywood.sloan@yahoo.com>
2022-11-15 15:08:00 -0500

    finish styling on messages

--- src/components/messenger/chatView/messageEntry/MessageEntry.tsx
+++ src/components/messenger/chatView/messageEntry/MessageEntry.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 
+import Avatar from 'src/components/core/avatar';
 import colors from 'src/styles/colors.module.scss';
 import fonts from 'src/styles/fonts.module.scss';
 import layout from 'src/styles/layout.module.scss';
@@ -21,26 +22,38 @@ export default function MessageEntry({
     <div
       className={classNames(
         styles.container,
-        layout.largePadding,
         layout.extraLargeMarginHorizontal,
         clusterEnd ? layout.extraLargeMarginBottom : layout.smallMarginBottom,
         {
           [styles.fromSelf]: fromSelf,
-          [styles.clusterEnd]: clusterEnd,
-          [styles.clusterStart]: clusterStart,
         }
       )}
     >
-      <span>Hey</span>
-      <div
-        className={classNames(
-          styles.date,
-          colors.textSecondary,
-          fonts.extraSmallSize,
-          layout.mediumMarginTop
+      {!fromSelf && (
+        <div className={styles.avatar}>{clusterEnd && <Avatar />}</div>
+      )}
+      <div>
+        <div
+          className={classNames(styles.bubble, layout.largePadding, {
+            [styles.clusterEnd]: clusterEnd,
+            [styles.clusterStart]: clusterStart,
+          })}
+        >
+          Hey
+        </div>
+        {clusterEnd && (
+          <div
+            className={classNames(
+              styles.date,
+              colors.textSecondary,
+              fonts.extraSmallSize,
+              layout.smallMarginTop,
+              layout.mediumMarginLeft
+            )}
+          >
+            10:00 AM, Nov 11
+          </div>
         )}
-      >
-        10:00 AM, Nov 11
       </div>
     </div>
   );
